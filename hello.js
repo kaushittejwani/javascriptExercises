95c2392019b212540552b605bcf6982ea1cd9d1f
riya-deligence <118877655+riya-deligence@users.noreply.github.com>
2022-11-28 09:54:20 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+<html>
+    <body>
+    
+<script>let person = prompt("please enter your name","harry")
+if(person!=null)
+{
+    document.getElementById("demo").innerHTML="hello"+ person + "how are you";
+}
+</script>
+</body>
+</html>
