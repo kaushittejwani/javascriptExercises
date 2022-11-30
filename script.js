3162cff74b556d6a252f9d2792213916e0ad4ca3
riya-deligence <riya@deligence.com>
2022-11-30 19:03:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let a = Math.floor(Math.random() * 20)
+//document.write(a)
+let c = ["What do you call a sleeping bull?A bulldozer.",
+    "Why do melons have weddings?They cantaloupe.",
+    "How do you make a tissue dance?You put a little boogie in it.",
+    "Why did the photo go to jail?It was framed.",
+    "Why did the golfer bring an extra pair of pants?In case he got a hole in one.",
+    "Why did the baby strawberry cry?His parents were in a jam.",
+    "Why did the scarecrow win an award?He was outstanding in his field.",
+    "What kind of jewelry do rabbits wear?14 carrot gold.",
+    "Where do polar bears keep their money?In a snowbank.",
+    "What do you call a factory that makes okay products?A satis-factory.",
+    "What did the ocean say to the sand?Nothing — it just waved.",
+    "Why couldn’t the sailor learn his alphabet?He kept getting lost at C.",
+    "What do lawyers wear to court?Lawsuits.", "What do cows most like to read?Cattle-logs.",
+    "What do you call an unpredictable camera?A loose Canon.",
+    "Why did the invisible man turn down the job offer?He couldn’t see himself doing it.",
+    "Why can’t a nose be 12 inches long?Then it’d be a foot.",
+    "Why is the grass so dangerous?It’s full of blades.",
+    "how do you fix a cracked pumpkin?With a pumpkin patch.",
+    "What does the dentist of the year get?A little plaque."]
+document.getElementById("jokes").innerHTML = c[a]
